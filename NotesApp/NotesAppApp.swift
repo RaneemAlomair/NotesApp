ccbe3e2ecb665622be0ac374fb66425045a183ef
Raneem Alomair <r.alomair.trn@ncgr.gov.sa>
2025-10-20 12:36:56 +0300

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Raneem Alomair on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by Raneem Alomair on 20/10/2025.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
